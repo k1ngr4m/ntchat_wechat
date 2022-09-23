7a231f9aae636d8a94cdd1830b8ee86106d1a3f1
k1ngr4m <365717424@qq.com>
2022-09-23 11:14:30 +0800

    20220923 update：优化代码

--- csgo/csgo.py
+++ csgo/csgo.py
@@ -14,12 +14,11 @@ from base.base import BaseFunc
 """
 
 
-
 class Csgo(BaseFunc):
     def __init__(self):
         super().__init__()
         self.key_money = 16
-        self.gubao_money = 4225     # 2016年科隆锦标赛古堡激战纪念包
+        self.gubao_money = 4225  # 2016年科隆锦标赛古堡激战纪念包
 
     def rand_weapon_from_json(self, case_name, signal):
         # self.excel_to_json()
@@ -106,6 +105,106 @@ class Csgo(BaseFunc):
             # print(weapon_content)
             return weapon_content
 
+    def rand_collection_weapon_from_json(self, case_name, singal):
+        # self.excel_to_json()
+        print('rand_collection')
+        with open(r'C:\py\git\PythonProject\ntchat_wechat\csgo\collection_case.json', encoding='utf-8') as a:
+            result = json.load(a)
+            is_find = False
+            for j in range(len(result)):
+                case_name_res = result[j]['case_name']
+                if case_name_res == case_name:
+                    contains = result[j]['contains']
+                    is_find = True
+                else:
+                    pass
+            if not is_find:
+                print('输入名称有误')
+                jsons = {'weapon_name': '输入名称有误！'}
+                return jsons
+            white_list = []
+            lightblue_list = []
+            blue_list = []
+            purple_list = []
+            pink_list = []
+            red_list = []
+
+            for i in range(len(contains)):
+                # print("xunhuan")
+                type = contains[i]['type']
+                if type == 'white':
+                    white_list.append(contains[i])
+                elif type == 'lightblue':
+                    lightblue_list.append(contains[i])
+                elif type == 'blue':
+                    blue_list.append(contains[i])
+                elif type == 'purple':
+                    purple_list.append(contains[i])
+                elif type == 'pink':
+                    pink_list.append(contains[i])
+                elif type == 'red':
+                    red_list.append(contains[i])
+                else:
+                    pass
+            a.close()
+            # print('列表创建完毕')
+            if singal:
+                rand_percent = random.uniform(0.9601, 1)
+            else:
+                rand_percent = random.random()
+            weapon_content = {}
+            print(rand_percent)
+            # 消费（白）
+            if rand_percent <= 0.8000:
+                randint = random.randint(0, len(white_list) - 1)
+                weapon_content['weapon_name'] = white_list[randint]['name']
+                lowest_price = white_list[randint]['lowest_price']
+                highest_price = white_list[randint]['highest_price']
+                weapon_content['weapon_price'] = round(random.uniform(lowest_price, highest_price), 2)
+                weapon_content['weapon_type'] = '白'
+            # 工业（浅蓝）
+            elif 0.8000 < rand_percent <= 0.9600:
+                randint = random.randint(0, len(lightblue_list) - 1)
+                weapon_content['weapon_name'] = lightblue_list[randint]['name']
+                lowest_price = lightblue_list[randint]['lowest_price']
+                highest_price = lightblue_list[randint]['highest_price']
+                weapon_content['weapon_price'] = round(random.uniform(lowest_price, highest_price), 2)
+                weapon_content['weapon_type'] = '浅蓝'
+            # 军规（蓝）
+            elif 0.9600 < rand_percent <= 0.9920:
+                randint = random.randint(0, len(blue_list) - 1)
+                weapon_content['weapon_name'] = blue_list[randint]['name']
+                lowest_price = blue_list[randint]['lowest_price']
+                highest_price = blue_list[randint]['highest_price']
+                weapon_content['weapon_price'] = round(random.uniform(lowest_price, highest_price), 2)
+                weapon_content['weapon_type'] = '蓝'
+            # 受限（紫）
+            elif 0.9920 < rand_percent <= 0.9984:
+                randint = random.randint(0, len(purple_list) - 1)
+                weapon_content['weapon_name'] = purple_list[randint]['name']
+                lowest_price = purple_list[randint]['lowest_price']
+                highest_price = purple_list[randint]['highest_price']
+                weapon_content['weapon_price'] = round(random.uniform(lowest_price, highest_price), 2)
+                weapon_content['weapon_type'] = '紫'
+            # 保密（粉）
+            elif 0.9984 < rand_percent <= 0.9996:
+                randint = random.randint(0, len(pink_list) - 1)
+                weapon_content['weapon_name'] = pink_list[randint]['name']
+                lowest_price = pink_list[randint]['lowest_price']
+                highest_price = pink_list[randint]['highest_price']
+                weapon_content['weapon_price'] = round(random.uniform(lowest_price, highest_price), 2)
+                weapon_content['weapon_type'] = '粉'
+            # 隐秘（红）
+            else:
+                randint = random.randint(0, len(red_list) - 1)
+                weapon_content['weapon_name'] = red_list[randint]['name']
+                lowest_price = red_list[randint]['lowest_price']
+                highest_price = red_list[randint]['highest_price']
+                weapon_content['weapon_price'] = round(random.uniform(lowest_price, highest_price), 2)
+                weapon_content['weapon_type'] = '红'
+            # print(weapon_content)
+            return weapon_content
+
     def open_case_from_json(self, case_count, case_name, single_case_money, signal=0):
         lists = []
         price = 0.00
@@ -145,32 +244,45 @@ class Csgo(BaseFunc):
         # print(pic_url)
         return msg, diff
 
-    # def excel_to_json(self):
-    #     data = pd.read_excel('./case_data.xls', sheet_name='Sheet1')
-    #     json_list = []
-    #     for case_name in data['case_name'].unique():
-    #         case_dict = {'case_name': case_name, 'contains': []}
-    #         case_data = data[data['case_name'] == case_name]
-    #
-    #         for kind in case_data['name'].unique():
-    #             info = {'name': kind}
-    #             kind_data = case_data[case_data['name'] == kind]
-    #
-    #             for type in kind_data['type'].unique():
-    #                 info['type'] = type
-    #
-    #             for low in kind_data['lowest_price'].unique():
-    #                 info['lowest_price'] = low
-    #
-    #             for high in kind_data['highest_price'].unique():
-    #                 info['highest_price'] = high
-    #             case_dict['contains'].append(info)
-    #         json_list.append(case_dict)
-    #
-    #     print(json_list)
-    #     data_dict = json.dumps(json_list, ensure_ascii=False)
-    #     with open('case.json', 'w', encoding='utf-8') as f_w:
-    #         f_w.write(data_dict)
+    def open_collection_case_from_json(self, case_count, case_name, single_case_money, signal=0):
+        lists = []
+        price = 0.00
+        # print('进来了')
+        # print(type(case_count))
+        for i in range(0, case_count):
+            # print(f'正在开第{i+1}')
+            weapon_content = self.rand_collection_weapon_from_json(case_name, signal)
+            # print('chu')
+            if weapon_content['weapon_name'] == '输入名称有误！':
+                return weapon_content['weapon_name']
+            # print(weapon_content)
+
+            weapon_price = weapon_content['weapon_price']
+            price = round(price + weapon_price, 2)
+            # print('有价格了')
+            weapon_content.pop('weapon_price')
+            lists.append(weapon_content)
+
+        nl = []
+        tl = [str(r) for r in lists]
+        for record in set(tl):
+            n = eval(record)
+            n.update({"rqs": tl.count(record)})
+            nl.append(n)
+        cost = single_case_money * case_count
+        diff = round(price - cost, 2)
+        msg = '抽到了：\n'
+        for i in range(len(nl)):
+            if nl[i]['weapon_type'] != '金':
+                msg = msg + f'{nl[i]["rqs"]} × 【{nl[i]["weapon_type"]}】：“{nl[i]["weapon_name"]}”\n'
+            else:
+                msg = msg + f'恭喜你，开到了一件极其罕见的物品“{nl[i]["weapon_name"]}”！！\n'
+        if diff >= 0:
+            msg = msg + f'此次您共花费了{cost}元，所开出的武器价值估算为{price}元，共盈利{diff}元'
+        else:
+            msg = msg + f'此次您共花费了{cost}元，所开出的武器价值估算为{price}元，共亏损{abs(diff)}元'
+        print(msg)
+        return msg, diff
 
     def open_cases(self, from_wxid, case_count, case_name):
         try:
@@ -197,10 +309,12 @@ class Csgo(BaseFunc):
                                     msg_1, diff = self.open_collection_case_from_json(case_count, case_name,
                                                                                       single_case_money, signal=1)
                                 else:
-                                    msg_1, diff = self.open_collection_case_from_json(case_count, case_name, single_case_money)
+                                    msg_1, diff = self.open_collection_case_from_json(case_count, case_name,
+                                                                                      single_case_money)
                             else:
                                 if from_wxid == self.xf_id:
-                                    msg_1, diff = self.open_case_from_json(case_count, case_name, single_case_money, signal = 1)
+                                    msg_1, diff = self.open_case_from_json(case_count, case_name, single_case_money,
+                                                                           signal=1)
                                 else:
                                     msg_1, diff = self.open_case_from_json(case_count, case_name, single_case_money)
                             remaining_money = round(money + diff, 2)
@@ -283,145 +397,9 @@ class Csgo(BaseFunc):
         except Exception as e:
             print(e)
 
-    def rand_collection_weapon_from_json(self, case_name, singal):
-        # self.excel_to_json()
-        print('rand_collection')
-        with open(r'C:\py\git\PythonProject\ntchat_wechat\csgo\collection_case.json', encoding='utf-8') as a:
-            result = json.load(a)
-            is_find = False
-            for j in range(len(result)):
-                case_name_res = result[j]['case_name']
-                if case_name_res == case_name:
-                    contains = result[j]['contains']
-                    is_find = True
-                else:
-                    pass
-            if not is_find:
-                print('输入名称有误')
-                jsons = {'weapon_name': '输入名称有误！'}
-                return jsons
-            white_list = []
-            lightblue_list = []
-            blue_list = []
-            purple_list = []
-            pink_list = []
-            red_list = []
 
-            for i in range(len(contains)):
-                # print("xunhuan")
-                type = contains[i]['type']
-                if type == 'white':
-                    white_list.append(contains[i])
-                elif type == 'lightblue':
-                    lightblue_list.append(contains[i])
-                elif type == 'blue':
-                    blue_list.append(contains[i])
-                elif type == 'purple':
-                    purple_list.append(contains[i])
-                elif type == 'pink':
-                    pink_list.append(contains[i])
-                elif type == 'red':
-                    red_list.append(contains[i])
-                else:
-                    pass
-            a.close()
-            # print('列表创建完毕')
-            if singal:
-                rand_percent = random.uniform(0.9601,1)
-            else:
-                rand_percent = random.random()
-            weapon_content = {}
-            print(rand_percent)
-            # 消费（白）
-            if rand_percent <= 0.8000:
-                randint = random.randint(0, len(white_list) - 1)
-                weapon_content['weapon_name'] = white_list[randint]['name']
-                lowest_price = white_list[randint]['lowest_price']
-                highest_price = white_list[randint]['highest_price']
-                weapon_content['weapon_price'] = round(random.uniform(lowest_price, highest_price), 2)
-                weapon_content['weapon_type'] = '白'
-            # 工业（浅蓝）
-            elif 0.8000 < rand_percent <= 0.9600:
-                randint = random.randint(0, len(lightblue_list) - 1)
-                weapon_content['weapon_name'] = lightblue_list[randint]['name']
-                lowest_price = lightblue_list[randint]['lowest_price']
-                highest_price = lightblue_list[randint]['highest_price']
-                weapon_content['weapon_price'] = round(random.uniform(lowest_price, highest_price), 2)
-                weapon_content['weapon_type'] = '浅蓝'
-            # 军规（蓝）
-            elif 0.9600 < rand_percent <= 0.9920:
-                randint = random.randint(0, len(blue_list) - 1)
-                weapon_content['weapon_name'] = blue_list[randint]['name']
-                lowest_price = blue_list[randint]['lowest_price']
-                highest_price = blue_list[randint]['highest_price']
-                weapon_content['weapon_price'] = round(random.uniform(lowest_price, highest_price), 2)
-                weapon_content['weapon_type'] = '蓝'
-            # 受限（紫）
-            elif 0.9920 < rand_percent <= 0.9984:
-                randint = random.randint(0, len(purple_list) - 1)
-                weapon_content['weapon_name'] = purple_list[randint]['name']
-                lowest_price = purple_list[randint]['lowest_price']
-                highest_price = purple_list[randint]['highest_price']
-                weapon_content['weapon_price'] = round(random.uniform(lowest_price, highest_price), 2)
-                weapon_content['weapon_type'] = '紫'
-            # 保密（粉）
-            elif 0.9984 < rand_percent <= 0.9996:
-                randint = random.randint(0, len(pink_list) - 1)
-                weapon_content['weapon_name'] = pink_list[randint]['name']
-                lowest_price = pink_list[randint]['lowest_price']
-                highest_price = pink_list[randint]['highest_price']
-                weapon_content['weapon_price'] = round(random.uniform(lowest_price, highest_price), 2)
-                weapon_content['weapon_type'] = '粉'
-            # 隐秘（红）
-            else:
-                randint = random.randint(0, len(red_list) - 1)
-                weapon_content['weapon_name'] = red_list[randint]['name']
-                lowest_price = red_list[randint]['lowest_price']
-                highest_price = red_list[randint]['highest_price']
-                weapon_content['weapon_price'] = round(random.uniform(lowest_price, highest_price), 2)
-                weapon_content['weapon_type'] = '红'
-            # print(weapon_content)
-            return weapon_content
 
-    def open_collection_case_from_json(self, case_count, case_name, single_case_money, signal=0):
-        lists = []
-        price = 0.00
-        # print('进来了')
-        # print(type(case_count))
-        for i in range(0, case_count):
-            # print(f'正在开第{i+1}')
-            weapon_content = self.rand_collection_weapon_from_json(case_name, signal)
-            # print('chu')
-            if weapon_content['weapon_name'] == '输入名称有误！':
-                return weapon_content['weapon_name']
-            # print(weapon_content)
 
-            weapon_price = weapon_content['weapon_price']
-            price = round(price + weapon_price, 2)
-            # print('有价格了')
-            weapon_content.pop('weapon_price')
-            lists.append(weapon_content)
-
-        nl = []
-        tl = [str(r) for r in lists]
-        for record in set(tl):
-            n = eval(record)
-            n.update({"rqs": tl.count(record)})
-            nl.append(n)
-        cost = single_case_money * case_count
-        diff = round(price - cost, 2)
-        msg = '抽到了：\n'
-        for i in range(len(nl)):
-            if nl[i]['weapon_type'] != '金':
-                msg = msg + f'{nl[i]["rqs"]} × 【{nl[i]["weapon_type"]}】：“{nl[i]["weapon_name"]}”\n'
-            else:
-                msg = msg + f'恭喜你，开到了一件极其罕见的物品“{nl[i]["weapon_name"]}”！！\n'
-        if diff >= 0:
-            msg = msg + f'此次您共花费了{cost}元，所开出的武器价值估算为{price}元，共盈利{diff}元'
-        else:
-            msg = msg + f'此次您共花费了{cost}元，所开出的武器价值估算为{price}元，共亏损{abs(diff)}元'
-        print(msg)
-        return msg, diff
 
     def sim(self):
         try:
@@ -436,7 +414,7 @@ class Csgo(BaseFunc):
                     msg = f'第{i}次开到龙狙啦！\n'
                     res.append(i)
 
-                    avg = int(sum(res)/len(res))
+                    avg = int(sum(res) / len(res))
                     msg = msg + f'大数据显示平均开到龙狙的次数为{avg}次'
 
                     with open(r'C:/statistics.json', 'w', encoding='utf-8') as st1:
@@ -463,7 +441,7 @@ class Csgo(BaseFunc):
                                 return msg
                             else:
                                 res[i]['money'] = round(money - number, 2)
-                                for j in range(len(res)-1):
+                                for j in range(len(res) - 1):
                                     if res[j]['from_wxid'] == user_wxid:
                                         money_1 = res[j]['money']
                                         res[j]['money'] = round(money_1 + number, 2)
@@ -477,3 +455,30 @@ class Csgo(BaseFunc):
         except Exception as e:
             print(e)
             return e
+
+    # def excel_to_json(self):
+    #     data = pd.read_excel('./case_data.xls', sheet_name='Sheet1')
+    #     json_list = []
+    #     for case_name in data['case_name'].unique():
+    #         case_dict = {'case_name': case_name, 'contains': []}
+    #         case_data = data[data['case_name'] == case_name]
+    #
+    #         for kind in case_data['name'].unique():
+    #             info = {'name': kind}
+    #             kind_data = case_data[case_data['name'] == kind]
+    #
+    #             for type in kind_data['type'].unique():
+    #                 info['type'] = type
+    #
+    #             for low in kind_data['lowest_price'].unique():
+    #                 info['lowest_price'] = low
+    #
+    #             for high in kind_data['highest_price'].unique():
+    #                 info['highest_price'] = high
+    #             case_dict['contains'].append(info)
+    #         json_list.append(case_dict)
+    #
+    #     print(json_list)
+    #     data_dict = json.dumps(json_list, ensure_ascii=False)
+    #     with open('case.json', 'w', encoding='utf-8') as f_w:
+    #         f_w.write(data_dict)
