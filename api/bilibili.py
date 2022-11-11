1476ef56a5527ab4f8552d095eccc6cb23bce1e2
k1ngr4m <365717424@qq.com>
2022-11-11 17:28:36 +0800

    20221111 update:send_broadcast_remind

--- api/bilibili.py
+++ api/bilibili.py
@@ -3,8 +3,8 @@ import requests
 from base.base import BaseFunc
 
 
-class Bilibili(BaseFunc):
-    def get_response(self, room_id):
+class Bilibili:
+    def get_room_response(self, room_id):
         try:
             url = f'https://api.live.bilibili.com/xlive/web-room/v2/index/getRoomPlayInfo?room_id={room_id}' \
                   f'&protocol=0,1&format=0,1,2&codec=0,1&qn=0&platform=web&ptype=8&dolby=5&panorama=1 '
@@ -16,8 +16,16 @@ class Bilibili(BaseFunc):
             # print(response)
             # return response
 
+    def get_room_info(self,room_id):
+        url = 'https://api.live.bilibili.com/xlive/web-room/v1/index/getOffLiveList?room_id=25059330&count=8&rnd=1668134243'
+        resonse = requests.get(url).json()
+        print(resonse)
+        data = resonse['data']
+        record_list = data['record_list']
+        print(record_list)
+
     def get_live_status(self, room_id):
-        response = self.get_response(room_id)
+        response = self.get_room_response(room_id)
         code = response['code']
         if code == 0:
             data = response['data']
@@ -47,5 +55,6 @@ if __name__ == '__main__':
     wb = Bilibili()
     roomid = 25059330
     roomids = 83171
-    wb.broadcast_remind(roomid)
-    wb.broadcast_remind(roomids)
+    # wb.broadcast_remind(roomid)
+    # wb.broadcast_remind(roomids)
+    wb.get_room_info(roomid)
