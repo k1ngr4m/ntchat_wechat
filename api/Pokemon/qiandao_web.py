d9743a2f979264d1bd94efecd8afef13c930b6ba
k1ngr4m <365717424@qq.com>
2022-11-22 14:15:31 +0800

    20221122 update:pokemon

--- api/Pokemon/qiandao_web.py
+++ api/Pokemon/qiandao_web.py
@@ -1,4 +1,5 @@
 import json
+import random
 import time
 
 from selenium import webdriver
@@ -85,6 +86,8 @@ class QiandaoWeb:
             img_5 = path + fr'\{name}_species_strength.png'
             species_strength.screenshot(path + fr'\{name}_species_strength.png')
 
+            self.roll_window_to_bottom(self.driver)
+
             attribute_restraint = detail_info.find_element(By.XPATH, '//*[@class="detail-info"]/div[6]')
             img_6 = path + fr'\{name}_attribute_restraint.png'
             attribute_restraint.screenshot(path + fr'\{name}_attribute_restraint.png')
@@ -136,6 +139,28 @@ class QiandaoWeb:
         joint.save(png_path)
         print(f"{name}拼图成功！")
         return png_path
+
+    def roll_window_to_bottom(self, browser, stop_length=None, step_length=500):
+        """selenium 滚动当前页面，向下滑
+        :param browser: selenium的webdriver
+        :param stop_length: 滑动的最大值
+        :param step_length: 每次滑动的值
+        """
+        original_top = 0
+        while True:  # 循环向下滑动
+            if stop_length:
+                if stop_length - step_length < 0:
+                    browser.execute_script("window.scrollBy(0,{})".format(stop_length))
+                    break
+                stop_length -= step_length
+
+            browser.execute_script("window.scrollBy(0,{})".format(step_length))
+            time.sleep(0.5 + random.random())  # 停顿一下
+            check_height = browser.execute_script(
+                "return document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;")
+            if check_height == original_top:  # 判断滑动后距顶部的距离与滑动前距顶部的距离
+                break
+            original_top = check_height
 
 
 if __name__ == '__main__':
